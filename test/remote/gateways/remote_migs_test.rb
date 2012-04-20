08e6e9b5e497c6a61479277dd144bb876286b4ad
Michael Noack <michael.noack@travellink.com.au>
2012-04-21 06:43:30 +0930

    Add test, comments, secure hash/offiste response methods and final cleanup

--- test/remote/gateways/remote_migs_test.rb
+++ test/remote/gateways/remote_migs_test.rb
@@ -2,20 +2,20 @@ require 'test_helper'
 require 'net/http'
 
 class RemoteMigsTest < Test::Unit::TestCase
-  
-
   def setup
-    @gateway = MigsGateway.new(fixtures(:migs))
-    
+    @gateway = MigsGateway.new(fixtures(:migs_purchase))
+    @capture_gateway = MigsGateway.new(fixtures(:migs_capture))
+    @sealink_gateway = MigsGateway.new(fixtures(:migs_sealink))
+
     @amount = 100
     @declined_amount = 105
-    @credit_card = credit_card('4005550000000001', :month => 5, :year => 2013)
-    @visa   = @credit_card
+    @visa   = credit_card('4005550000000001', :month => 5, :year => 2013, :type => 'visa')
     @master = credit_card('5123456789012346', :month => 5, :year => 2013, :type => 'master')
-    @amex   = credit_card('345678901234564', :month => 5, :year => 2013, :type => 'american_express')
-    @diners = credit_card('30123456789019', :month => 5, :year => 2013, :type => 'diners_club')
-    
-    @options = { 
+    @amex   = credit_card('371449635311004',  :month => 5, :year => 2013, :type => 'american_express')
+    @diners = credit_card('30123456789019',   :month => 5, :year => 2013, :type => 'diners_club')
+    @credit_card = @visa
+
+    @options = {
       :order_id => '1'
     }
   end
@@ -27,20 +27,20 @@ class RemoteMigsTest < Test::Unit::TestCase
       return_url: 'http://localhost:8080/payments/return'
     }
 
-    choice_url = @gateway.purchase_offsite_url(@amount, nil, options)
+    choice_url = @gateway.purchase_offsite_url(@amount, options)
     assert_response_contains 'Pay securely by clicking on the card logo below', choice_url
 
-    visa_url = @gateway.purchase_offsite_url(@amount, @visa, options)
-    assert_response_contains 'You have chosen <B>VISA</B>', visa_url
+    responses = {
+      'visa'             => 'You have chosen <B>VISA</B>',
+      'master'           => 'You have chosen <B>MasterCard</B>',
+      'diners_club'      => 'You have chosen <B>Diners Club</B>',
+      'american_express' => 'You have chosen <B>American Express</B>'
+    }
 
-    master_url = @gateway.purchase_offsite_url(@amount, @master, options)
-    assert_response_contains 'You have chosen <B>MasterCard</B>', master_url
-
-    diners_url = @gateway.purchase_offsite_url(@amount, @diners, options)
-    assert_response_contains 'You have chosen <B>Diners Club</B>', diners_url
-
-    amex_url = @gateway.purchase_offsite_url(@amount, @amex, options)
-    assert_response_contains 'You have chosen <B>American Express</B>', amex_url
+    responses.each_pair do |card_type, response_text|
+      url = @gateway.purchase_offsite_url(@amount, options.merge(:card_type => card_type))
+      assert_response_contains response_text, url
+    end
   end
 
   def test_successful_purchase
@@ -55,25 +55,24 @@ class RemoteMigsTest < Test::Unit::TestCase
     assert_equal 'Declined', response.message
   end
 
-  # Cannot test as test gateway does not allow auth/capture mode
-  #def test_authorize_and_capture
-  #  assert auth = @gateway.authorize(@amount, @credit_card, @options)
-  #  assert_success auth
-  #  assert_equal 'Approved', auth.message
-  #  assert capture = @gateway.capture(@amount, auth.params['TransactionNo'], @options)
-  #  assert_success capture
-  #end
-  #
-  #def test_failed_capture
-  #  assert response = @gateway.capture(@declined_amount, @credit_card, @options)
-  #  assert_failure response
-  #  assert_equal 'Declined', response.message
-  #end
+  def test_authorize_and_capture
+    assert auth = @capture_gateway.authorize(@amount, @credit_card, @options)
+    assert_success auth
+    assert_equal 'Approved', auth.message
+    assert capture = @capture_gateway.capture(@amount, auth.authorization, @options)
+    assert_success capture
+  end
+
+  def test_failed_authorize
+    assert response = @capture_gateway.authorize(@declined_amount, @credit_card, @options)
+    assert_failure response
+    assert_equal 'Declined', response.message
+  end
 
   def test_refund
     assert payment_response = @gateway.purchase(@amount, @credit_card, @options)
     assert_success payment_response
-    assert response = @gateway.refund(@amount, payment_response.params['TransactionNo'], @options)
+    assert response = @gateway.refund(@amount, payment_response.authorization, @options)
     assert_success response
     assert_equal 'Approved', response.message
   end
@@ -86,10 +85,7 @@ class RemoteMigsTest < Test::Unit::TestCase
   end
 
   def test_invalid_login
-    gateway = MigsGateway.new(
-                :login => '',
-                :password => ''
-              )
+    gateway = MigsGateway.new(:login => '', :password => '')
     assert response = gateway.purchase(@amount, @credit_card, @options)
     assert_failure response
     assert_equal 'Required field vpc_Merchant was not present in the request', response.message
@@ -115,5 +111,4 @@ class RemoteMigsTest < Test::Unit::TestCase
     end
     response
   end
-
 end
--- test/unit/gateways/migs_test.rb
+++ test/unit/gateways/migs_test.rb
@@ -5,7 +5,7 @@ class MigsTest < Test::Unit::TestCase
     @gateway = MigsGateway.new(
                  :login => 'login',
                  :password => 'password',
-                 :secure_hash => '0F5DD14AE2E38C7EBD8814D29CF6F6F0'
+                 :secure_hash => '76AF3392002D202A60D0AB5F9D81653C'
                )
 
     @credit_card = credit_card
@@ -49,6 +49,25 @@ class MigsTest < Test::Unit::TestCase
 
     @gateway.send(:add_secure_hash, params)
     assert_equal Digest::MD5.hexdigest(ordered_values).upcase, params[:SecureHash]
+  end
+
+  def test_purchase_offsite_response
+    # Below response from instance running remote test
+    response_params = "vpc_3DSXID=a1B8UcW%2BKYqkSinLQohGmqQd9uY%3D&vpc_3DSenrolled=U&vpc_AVSResultCode=Unsupported&vpc_AcqAVSRespCode=Unsupported&vpc_AcqCSCRespCode=Unsupported&vpc_AcqResponseCode=00&vpc_Amount=100&vpc_AuthorizeId=367739&vpc_BatchNo=20120421&vpc_CSCResultCode=Unsupported&vpc_Card=MC&vpc_Command=pay&vpc_Locale=en&vpc_MerchTxnRef=9&vpc_Merchant=TESTANZTEST3&vpc_Message=Approved&vpc_OrderInfo=1&vpc_ReceiptNo=120421367739&vpc_SecureHash=8794D9478D030B65F3092282E76283F8&vpc_TransactionNo=2000025183&vpc_TxnResponseCode=0&vpc_VerSecurityLevel=06&vpc_VerStatus=U&vpc_VerType=3DS&vpc_Version=1"
+
+    response_hash = @gateway.send(:parse, response_params)
+    response_hash.delete(:SecureHash)
+    calculated_hash = @gateway.send(:calculate_secure_hash, response_hash, @gateway.options[:secure_hash])
+    assert_equal '8794D9478D030B65F3092282E76283F8', calculated_hash
+
+    response = @gateway.purchase_offsite_response(response_params)
+    assert_success response
+
+    tampered_response1 = response_params.gsub('83F8', '93F8')
+    assert_raise(SecurityError){@gateway.purchase_offsite_response(tampered_response1)}
+
+    tampered_response2 = response_params.gsub('Locale=en', 'Locale=es')
+    assert_raise(SecurityError){@gateway.purchase_offsite_response(tampered_response2)}
   end
 
   private
